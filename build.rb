9c583e41cbd72e11a89b2bfe2d626cd36b80a491
Arun <arun.salian@gmail.com>
2021-08-11 11:48:14 +0530

    Update build.rb

--- build.rb
+++ build.rb
@@ -12,7 +12,7 @@ pipeline {
 
     stage ('Compile and package') {
       steps {
-        sh 'mvn clean package'
+        sh '/media/hd1/Xtras/apache-maven-3.8.1/bin/mvn clean package'
       }
     }
     
